df55dddf54be45948f92b97b86423d0df4705f34
Wim-Lotz <wimlotz@protonmail.com>
2024-09-25 16:48:40 +0200

    added dapper and postgres

---
+++ src/Todos.Application/Database/DbInitializer.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+
+namespace Todos.Application.Database;
+
+public class DbInitializer
+{
+    private readonly IDbConnectionFactory _dbConnectionFactory;
+
+    public DbInitializer(IDbConnectionFactory dbConnectionFactory)
+    {
+        _dbConnectionFactory = dbConnectionFactory;
+    }
+
+    public async Task InitializeAsync()
+    {
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+
+        await connection.ExecuteAsync($"""
+                                       create table if not exists todos (
+                                           id UUID primary key,
+                                           description text not null,
+                                           completedOn timestamp null,
+                                           completed boolean not null default false);
+                                       """);
+    }
+}
